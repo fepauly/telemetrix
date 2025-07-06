0c05f4d93f4365c5fe2d0b0ef1601b0c227e8e6a
fepauly <f.pauly97@gmail.com>
2025-07-06 10:16:40 +0200

    Setup DB and mqtt handling

--- apps/telemetrix/lib/telemetrix/application.ex
+++ apps/telemetrix/lib/telemetrix/application.ex
@@ -6,15 +6,26 @@ defmodule Telemetrix.Application do
   use Application
 
   @impl true
+  @spec start(any(), any()) :: {:error, any()} | {:ok, pid()}
   def start(_type, _args) do
     children = [
       Telemetrix.Repo,
       {Ecto.Migrator,
        repos: Application.fetch_env!(:telemetrix, :ecto_repos), skip: skip_migrations?()},
       {DNSCluster, query: Application.get_env(:telemetrix, :dns_cluster_query) || :ignore},
-      {Phoenix.PubSub, name: Telemetrix.PubSub}
+      {Phoenix.PubSub, name: Telemetrix.PubSub},
       # Start a worker by calling: Telemetrix.Worker.start_link(arg)
       # {Telemetrix.Worker, arg}
+      {Tortoise.Connection,
+        [
+          client_id: "telemetrix_dev",
+          server: {Tortoise.Transport.Tcp, host: "localhost", port: 1883},
+          handler: {Telemetrix.MQTT.Handler, []},
+          subscriptions: [
+            {"esp32/+/sensor/#", 0}
+          ]
+        ]
+      }
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one, name: Telemetrix.Supervisor)
---
+++ apps/telemetrix/lib/telemetrix/mqtt/handler.ex
@@ -0,0 +1,67 @@
+defmodule Telemetrix.MQTT.Handler do
+  use Tortoise.Handler
+  require Logger
+  alias Telemetrix.SensorReadings
+
+  def init(args) do
+    Logger.info("Initializing handler with args: #{inspect(args)}")
+    {:ok, args}
+  end
+
+  def connection(:up, state) do
+    Logger.info("MQTT connected")
+    {:ok, state}
+  end
+
+  def connection(:down, state) do
+    Logger.warning("MQTT disconnected")
+    {:ok, state}
+  end
+
+  def handle_message(["esp32", device_id, "sensor", type], payload, state) do
+    case parse_payload(payload) do
+      {:ok, value, timestamp} ->
+          case SensorReadings.ingest(device_id, type, value, timestamp) do
+            {:ok, _reading} -> :ok
+            {:error, changeset} ->
+              Logger.error("[DB][ERROR] #{inspect(changeset)} | #{device_id}/#{type}")
+          end
+
+      {:error, reason} ->
+        Logger.error("[MQTT][ERROR] #{reason} | #{inspect(payload)}")
+    end
+
+    {:ok, state}
+  end
+
+  def handle_message(topic_levels, payload, state) do
+    # We ingnore any other topic for now
+    {:ok, state}
+  end
+
+  def subscription(status, topic_filter, state) do
+    {:ok, state}
+  end
+
+  def terminate(reason, state) do
+    Logger.info("Terminating with reason: #{reason}")
+    {:ok, state}
+  end
+
+
+  def last_will(state) do
+    {{:ok, nil}, state}
+  end
+
+  defp parse_payload(payload) do
+    case Jason.decode(payload) do
+      {:ok, %{"value" => value, "timestamp" => ts}} ->
+        case DateTime.from_iso8601(ts) do
+          {:ok, dt, _} -> {:ok, value, dt}
+          error -> {:error, {:invalid_timestamp, error}}
+        end
+
+      error -> {:error, {:invalid_json, error}}
+    end
+  end
+end
